79e29084233ac97e95dba313dc0728d8db7520e0
Mark Beale <bealebeaverst@gmail.com>
2024-10-08 10:54:40 -0700

    reorganizing

--- server/config.py
+++ server/config.py
@@ -4,3 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///fantasy_tdf.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-secret-string'
