44c6802fa27598dfb7e9585d7eaee0c86c73d45e
Mark Beale <bealebeaverst@gmail.com>
2024-10-11 16:21:43 -0700

    working server and semi-client

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import App from "./App";
 import "./index.css";
 
-ReactDOM(
+// Initialize the root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component wrapped in the Redux Provider
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
