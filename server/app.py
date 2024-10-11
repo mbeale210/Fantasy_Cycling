a2600c3ded1def975400b2934e7f5b28abd5ea96
Mark Beale <bealebeaverst@gmail.com>
2024-10-11 02:03:58 -0700

    port update

--- server/app.py
+++ server/app.py
@@ -6,3 +6,6 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Rider': Rider, 'Stage': Stage, 'StageResult': StageResult}
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS
 from config import Config
 
 db = SQLAlchemy()
@@ -16,10 +15,8 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
-    from .models import User, Rider, FantasyTeam, Stage, StageResult, League
-    from .routes import auth, teams, riders, stages, leagues
+    from app.routes import auth, teams, riders, stages, leagues
     app.register_blueprint(auth.bp)
     app.register_blueprint(teams.bp)
     app.register_blueprint(riders.bp)
